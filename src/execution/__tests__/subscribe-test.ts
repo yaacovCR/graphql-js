d4c0b896077d9d31180963bee7c1d89ac999ef57
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-06-22 17:54:40 +0300

    use named arguments for buildExecutionContext

--- src/execution/__tests__/subscribe-test.ts
+++ src/execution/__tests__/subscribe-test.ts
@@ -410,7 +410,7 @@ describe('Subscription Initialization Phase', () => {
       const document = parse('subscription { foo }');
       const result = await subscribe({ schema, document });
 
-      const exeContext = buildExecutionContext(schema, document);
+      const exeContext = buildExecutionContext({ schema, document });
       expect(await createSourceEventStream(exeContext)).to.deep.equal(result);
       return result;
     }
